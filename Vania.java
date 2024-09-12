9a65aa93b8b40c4e9dca8a604a650e6bff577454
Vania Acevedo <vania.acevedo@usach.cl>
2024-09-12 19:11:23 -0300

    Create Vania.java

---
+++ Vania.java
@@ -0,0 +1,9 @@
+package org.emaldonado.apiservlet.webapp.tarea.headers;
+
+public class Vania {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Tio Edin");
+
+    }
+}
